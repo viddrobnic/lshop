f68b1f39190c7ff41532a81fe8b81f58b8693647
Vid Drobnič <me@viddrobnic.com>
2025-12-22 21:49:02 +0100

    users migration

--- backend/migrations/20251222133700_initial.sql
+++ backend/migrations/20251222133700_initial.sql
@@ -1,1 +1,16 @@
-CREATE TABLE test(id INT PRIMARY KEY, value TEXT) STRICT;
+CREATE TABLE users (
+    id            INT PRIMARY KEY,
+    username      TEXT NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at    TEXT NOT NULL,
+    updated_at    TEXT NOT NULL
+) STRICT;
+
+CREATE TABLE user_sessions (
+    user_id      INT NOT NULL REFERENCES users(id) ON DELETE CASCADE, 
+    session_hash TEXT NOT NULL,
+    expires_at   TEXT NOT NULL,
+    created_at   TEXT NOT NULL
+) STRICT;
+
+CREATE INDEX user_session_hash_idx ON user_sessions(session_hash);
